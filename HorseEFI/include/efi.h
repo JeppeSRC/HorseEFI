960ed2de7d01c716bebfe576d031cff4f3ebee99
JeppeSRC <jeppe97rc@gmail.com>
2018-03-12 19:24:08 +0100

    Added EFI_FILE*_INFO

--- HorseEFI/include/efi.h
+++ HorseEFI/include/efi.h
@@ -1372,3 +1372,50 @@ typedef struct EFI_FILE_PROTOCOL {
 	EFI_FILE_WRITE_EX WriteEx; // Added for revision 2
 	EFI_FILE_FLUSH_EX FlushEx; // Added for revision 2
 } EFI_FILE_PROTOCOL;
+
+#define EFI_FILE_INFO_ID \
+ {0x09576e92,0x6d3f,0x11d2,\
+ {0x8e39,0x00,0xa0,0xc9,0x69,0x72,0x3b}}
+
+//*******************************************************
+// File Attribute Bits
+//*******************************************************
+#define EFI_FILE_READ_ONLY 0x0000000000000001
+#define EFI_FILE_HIDDEN 0x0000000000000002
+#define EFI_FILE_SYSTEM 0x0000000000000004
+#define EFI_FILE_RESERVED 0x0000000000000008
+#define EFI_FILE_DIRECTORY 0x0000000000000010
+#define EFI_FILE_ARCHIVE 0x0000000000000020
+#define EFI_FILE_VALID_ATTR 0x0000000000000037
+
+typedef struct {
+	UINT64 Size;
+	UINT64 FileSize;
+	UINT64 PhysicalSize;
+	EFI_TIME CreateTime;
+	EFI_TIME LastAccessTime;
+	EFI_TIME ModificationTime;
+	UINT64 Attribute;
+	CHAR16* FileName;
+} EFI_FILE_INFO;
+
+#define EFI_FILE_SYSTEM_INFO_ID \
+ {0x09576e93,0x6d3f,0x11d2,0x8e39,0x00,0xa0,0xc9,0x69,0x72,\
+ 0x3b}
+
+typedef struct {
+	UINT64 Size;
+	BOOLEAN ReadOnly;
+	UINT64 VolumeSize;
+	UINT64 FreeSpace;
+	UINT32 BlockSize;
+	CHAR16* VolumeLabel;
+} EFI_FILE_SYSTEM_INFO;
+
+#define EFI_FILE_SYSTEM_VOLUME_LABEL_ID \
+{0xdb47d7d3,0xfe81,0x11d3,0x9a35,\
+ {0x00,0x90,0x27,0x3f,0xC1,0x4d}}
+
+typedef struct {
+	CHAR16* VolumeLabel;
+} EFI_FILE_SYSTEM_VOLUME_LABEL;
